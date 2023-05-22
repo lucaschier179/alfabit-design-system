287a652c9cb70981acf48a6d42c652e2a9ae7ba1
cardozov <vinicius.gcardozo@gmail.com>
2023-05-22 19:10:41 -0300

    aula 5 - video 4

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -6,11 +6,25 @@ export type InputProps = {
   React.TextareaHTMLAttributes<HTMLTextAreaElement>
 )
 
-const Input = (props: InputProps) => {
+const InputOrTextArea = (props: InputProps) => {
   if (props.multiline) {
     return <textarea {...props as React.TextareaHTMLAttributes<HTMLTextAreaElement>} />
   }
   return <input {...props as React.InputHTMLAttributes<HTMLInputElement>} />
+}
+
+const Input = ({ label, className, ...rest}: InputProps) => {
+  return <div className="relative">
+    {label && <label className="absolute">{label}</label>}
+    <InputOrTextArea
+      className={`
+        bg-dark text-gray-primary
+        disabled:bg-disabled disabled:border-2 disabled:border-disabled
+        ${className}
+      `}
+      {...rest}
+    />
+  </div>
 }
 
 export default Input
