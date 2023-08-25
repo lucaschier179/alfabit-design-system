8168e75ae4e9cc38d753573c30fd0620203b18ef
lucaschier <ens-lucasschier@ugv.edu.br>
2023-08-25 08:33:05 -0300

    Aula - Trabalhando com cores

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,6 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode,
 }
 
-
 export default function Button({ children, ...rest }: ButtonProps) {
   return (
     <button className={twMerge("flex items-center py-3 px-8 rounded-lg font-bold text-white bg-greenPormade", rest.className)}> {/* Estilização base/padrão do botão */}
